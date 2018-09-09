01ff2bea41ab6374e498b1a78387f0dc9b8b2737
Ashish Narasimham <ashish.narasimham@gmail.com>
2018-09-09 09:44:01 -0400

    insert transaction_customer data

--- hbase/hbase-java/src/main/java/com/ervits/hbase/HBaseAPI.java
+++ hbase/hbase-java/src/main/java/com/ervits/hbase/HBaseAPI.java
@@ -5,13 +5,19 @@
  */
 package com.ervits.hbase;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
@@ -28,6 +34,8 @@ import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.RetriesExhaustedWithDetailsException;
 import org.apache.hadoop.hbase.io.compress.Compression.Algorithm;
 import org.apache.hadoop.hbase.util.Bytes;
+
+import com.opencsv.CSVReader;
 
 /**
  *
@@ -111,28 +119,46 @@ public class HBaseAPI {
         };
         BufferedMutatorParams params = new BufferedMutatorParams(tableName)
                 .listener(listener);
+        FileSystem fs;
+		
         try (Connection connection = ConnectionFactory.createConnection(config);
                 final BufferedMutator mutator = connection.getBufferedMutator(params)) {
+	        	fs = FileSystem.get(config);
 
-            List<Put> puts = new ArrayList<>();
-            int count = 0;
-            Put put;
+	        	FileStatus[] fileStatus = fs.listStatus(new Path("hdfs://anarasimham-hdp-1.field.hortonworks.com:8020/tmp/txn_new"));
 
-            for (int i = 0; i < 1000000; i++) {
-                String rowKey = UUID.randomUUID().toString();
+	        	for (FileStatus st : fileStatus) {
+	        		Path filePath = st.getPath();
+	        		int count = 0;
+	        		Put put;
+	        		BufferedReader buf = new BufferedReader(new InputStreamReader(fs.open(filePath)));
+	        		List<Put> puts = new ArrayList<>();
+	        		CSVReader csv = new CSVReader(buf);
+	        		
+	        		while (buf.ready()) {
+	        			String[] line = csv.readNext();
+	        			String rowKey = String.join("~", line[7], line[6], line[1], line[4], line[3]);
+	        			put = new Put(Bytes.toBytes(rowKey));
+	        			put.addColumn(Bytes.toBytes(CF_DEFAULT), Bytes.toBytes("trxn_amt"), Bytes.toBytes(line[8]));
+	        			put.addColumn(Bytes.toBytes(CF_DEFAULT), Bytes.toBytes("discount_amt"), Bytes.toBytes(line[9]));
+	        			put.addColumn(Bytes.toBytes(CF_DEFAULT), Bytes.toBytes("store_id"), Bytes.toBytes(line[10]));
+	        			put.addColumn(Bytes.toBytes(CF_DEFAULT), Bytes.toBytes("rep_id"), Bytes.toBytes(line[11]));
+	        			put.addColumn(Bytes.toBytes(CF_DEFAULT), Bytes.toBytes("part_sku"), Bytes.toBytes(line[12]));
+	        			put.addColumn(Bytes.toBytes(CF_DEFAULT), Bytes.toBytes("id"), Bytes.toBytes(line[0]));
+	        			put.addColumn(Bytes.toBytes(CF_DEFAULT), Bytes.toBytes("qty"), Bytes.toBytes(line[13]));
+	        			puts.add(put);
+	        			count++;
+	        			if((count % 10000) == 0) {
+        					mutator.mutate(puts);
+        					LOG.log(Level.INFO, "Count: {0}", count);
+        					puts.clear();
+        				}
+	        		}
+	        		mutator.mutate(puts);
+	        		puts.clear();
+	        	}
+	        	
 
-                put = new Put(Bytes.toBytes(rowKey));
-                put.addColumn(Bytes.toBytes(CF_DEFAULT), Bytes.toBytes("cnt"), Bytes.toBytes(count));
-                puts.add(put);
-                count++;
-                
-                if((count % 10000) == 0) {
-                    mutator.mutate(puts);
-                    LOG.log(Level.INFO, "Count: {0}", count);
-                    puts.clear();
-                }
-            }
-            mutator.mutate(puts);
             
 //            String rowKey = UUID.randomUUID().toString();
 //            Put put = new Put(Bytes.toBytes(rowKey));
@@ -144,19 +170,20 @@ public class HBaseAPI {
 //                
 //            }
 //            mutator.mutate(put);
-            
 
-        } catch (IOException ex) {
+        } catch (IllegalArgumentException | IOException ex) {
             LOG.log(Level.SEVERE, ex.getMessage());
+            ex.printStackTrace();
         }
     }
 
     public static void main(String... args) throws IOException {
         Configuration config = HBaseConfiguration.create();
-
         config.set("hbase.zookeeper.quorum", "anarasimham-hdp-1.field.hortonworks.com");
         config.set("hbase.zookeeper.property.clientPort", "2181");
         config.set("zookeeper.znode.parent", "/hbase-unsecure");
+        config.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+        config.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
         
         //Add any necessary configuration files (hbase-site.xml, core-site.xml)
 
